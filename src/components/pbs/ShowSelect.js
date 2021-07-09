89214af664129f383860cc906b353ddc91adda44
baden allen <baden.j.allen@gmail.com>
2021-07-09 10:53:03 +1000

    fucked, but about to try something

--- src/components/pbs/ShowSelect.js
+++ src/components/pbs/ShowSelect.js
@@ -18,55 +18,56 @@ const Showselect = () => {
 
   
     useEffect(() => {
-        console.log("useEffect 1")
+        // console.log("useEffect 1")
         getShowList();
         // eslint-disable-next-line
         }, []);
 
     useEffect(() => {
-        console.log("useEffect 2")
+        // console.log("useEffect 2")
         getEpisodeList();
         // eslint-disable-next-line
         }, [selectedShow]);
  
     useEffect(() => {
-        console.log("useEffect 3")
+        // console.log("useEffect 3")
         setSongList([]);
         getSongList();
         // eslint-disable-next-line
         }, [episodes]);
 
-    useEffect(() => {
-        console.log("useEffect 4")
-        let tempSongList = songList;
+    // useEffect(() => {
+    //     console.log("useEffect 4")
+    //     let tempSongList = songList;
 
-        tempSongList.sort(function (a, b) {
-            return new Date(b.date) - new Date(a.date);
-        });
-        // console.log(1, tempSongList.length)
-        // tempSongList.slice(songCount);
-        // console.log(2, tempSongList.length)
-        setSongList(tempSongList);
-        // eslint-disable-next-line
-        }, [songList]);
+    //     tempSongList.sort(function (a, b) {
+    //         return new Date(b.date) - new Date(a.date);
+    //     });
+    //     // setSongList(tempSongList.slice(0, 15));
+    //     // eslint-disable-next-line
+    //     }, [songList, setSongList]);
         
 
     // Get a list of Pbs Shows, called by useEffect on render.
     const getShowList = async () => {
         let ShowList = [{id: 0, name: 'Select a PBS Show', url: null}];
-        const res = await axios
-        .get('https://airnet.org.au/rest/stations/3pbs/programs');
-        res.data.forEach((program, index) => {
-            if(program.programRestUrl !== "https://airnet.org.au/rest/stations/3pbs/programs/"){
-            //Create ShowList
-            ShowList = [...ShowList, {
-                id: index,
-                name: program.name, 
-                url: program.programRestUrl
-            }];
-            };
-        setShowList(ShowList)
-        });
+        try{
+            const res = await axios.get('https://airnet.org.au/rest/stations/3pbs/programs');
+            res.data.forEach((program, index) => {
+                if(program.programRestUrl !== "https://airnet.org.au/rest/stations/3pbs/programs/"){
+                    //Create ShowList
+                    ShowList = [...ShowList, {
+                        id: index,
+                        name: program.name, 
+                        url: program.programRestUrl
+                    }];
+                };
+            setShowList(ShowList)
+            });
+        }catch(e) {
+            console.error('PBS Show List Query', e);
+        }
+        
     };
 
 
@@ -75,64 +76,96 @@ const Showselect = () => {
         if (selectedShow.url != null){
             let episodeList = [];
             //Loop through all episodes episodes of selected show
-            axios.get(`${selectedShow.url}/episodes?numAfter=${episodeCount}&numBefore=${episodeCount}`)
-            .then(function (response) {
-                response.data.forEach((episode) => {
+            try{
+                let res = await axios.get(`${selectedShow.url}/episodes?numAfter=${episodeCount}&numBefore=${episodeCount}`)
+                res.data.forEach((episode) => {
                     episodeList = [...episodeList, episode];
                 })
-                //Sort episodes by date (newest first)
+                // Sort episodes by date (newest first)
                 episodeList.sort(function (a, b) {
                     return new Date(b.start) - new Date(a.start);
-                  });
+                    });
                 setEpisodes(episodeList);
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            })
+
+            } catch(e) {
+                console.error('Episode List Query', e);
+            }
         };
   };
 
-  const getSongList = async () =>{
+  
+  // So what i want is one songlist for the selected show, but ordered by the most recent episode first.
+  // The issue im having is that declaring vairable to hold the data at the start of the function dosen't seem
+  // to scope, and even if the list is populated inside the loop, at the end of the loop when i want to sort 
+  // and save the list into state, the data is no longer there.
+  // there is also an issue here with async, so oftern an evaluation of a vairable at the bottom of the function will be
+  // executed before the completion of all the async calls. 
+  
+//So i think by using promise.all() i can find a solution. Basically i have to split this function into two 
+// an async function that returns a promise (similar to CT's wordGIF)
+// and a function that creates an array of these promises and resolves them in order.
+// The songlist will be the result of this second function
+  const getSongList = () =>{
+    console.log("start")
+    const getUsers = () => {
+        return new Promise((resolve, reject) => {
+          return setTimeout(
+            () => resolve([{ id: 'jon' }, { id: 'andrey' }, { id: 'tania' }]),
+            2000
+          )
+        })
+      }
+      const object1 = getUsers();
+      console.log(1, object1);
+    
+    const asyncTest = async () => {
+        const object2 = await getUsers();
+        console.log(2, object2);
+    }
+    
+    asyncTest();
 
-    // Function to clean up inputs from PBS playlists.
-    const cleanString = (string) =>{
-        string=string.split('-')[0]
-        string=string.split('(')[0]
-        string=string.split('+')[0]
-        string=string.split('[')[0]
-        string=string.split('ft.')[0]
-        string=string.split('feat.')[0]
-        string=string.split('feat')[0]
-        string=string.split('FT')[0]
+    
+
+    // Loop through playlist for each episode
+    // episodes.forEach((episode) => {
+    //     // Get request for playlist for each episode
+    //     let res = axios.get(`${episode.episodeRestUrl}/playlists`)
+    //     console.log('promise time', res.data)
         
-        // Just remove dont cut rest of string.
-        // string=string.split('|')[0]
-        return string;
-    };
+    // });
+    // try {
+    //     }catch(e) {
+    //         console.error('Song List Query', e);
+    //     }
+        
+    
+    // response.data.map((SongData) => (
+        //     SongList = [...SongList, {
+            //         id: SongData.id,
+            //         track: SongData.track, 
+            //         artist: SongData.artist,
+            //         date: episode.start
+            //     }]
+            // ));
 
-    let SongList = [];
-    //Loop through playlist for each episode
-    episodes.forEach((episode) => {
-        console.log("ep start:", episode.start);
-        //Get request for playlist for each episode
-        axios.get(`${episode.episodeRestUrl}/playlists`).then(function (response) {
-            response.data.map((SongData) => (
-                SongList = [...SongList, {
-                    id: SongData.id,
-                    track: cleanString(SongData.track), 
-                    artist: cleanString(SongData.artist),
-                    date: episode.start
-                }]
-                ));
-                setSongList(SongList);
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            })
-    });
+            // Function to clean up inputs from PBS playlists.
+            const cleanString = (string) =>{
+                string=string.split('-')[0]
+                string=string.split('(')[0]
+                string=string.split('+')[0]
+                string=string.split('[')[0]
+                string=string.split('ft.')[0]
+                string=string.split('feat.')[0]
+                string=string.split('feat')[0]
+                string=string.split('FT')[0]
+                
+                // Just remove dont cut rest of string.
+                // string=string.split('|')[0]
+                return string;
+            };
   };
+
 
 
   // Handler for Show Selection dropdown.
@@ -143,7 +176,7 @@ const Showselect = () => {
             setSelectedShow(show);
         }
     });
-};
+    };
 
     return (
         <div style={showSelectStyle}>
