3ae987b5bd7e2e0e04203670a83b01961750ae91
baden allen <baden.j.allen@gmail.com>
2023-07-01 16:21:18 +1200

    Removed unused components

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image';
 
 const About = () => {
